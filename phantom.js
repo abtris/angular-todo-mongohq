a26378ff89b05b5f283de84d1ca1340e871f7a8e
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2012-05-19 20:52:05 +0200

    Add docs target Update jasmine, remove qunit

--- phantom.js
+++ phantom.js
@@ -1,11 +1,13 @@
 console.log('Loading a web page');
 var page = new WebPage();
-var url = "http://angular.dev/";
+var url = "junit_xml_reporter.html";
 page.open(url, function (status) {
     var title = page.evaluate(function () {
          return document.title;
     });
     console.log('Page title is "' + title + '"');
-
+    var el = document.getElementById( "runner passed" );
+			// return ( el !== -1 );
+	console.log(el);
     phantom.exit();
 });
